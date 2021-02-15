7298d90933c49571aa4bdf175dd2e871cd47d789
nextweekscode <ashtoturn@gmail.com>
2021-02-15 12:25:12 -0600

    create user profile and create program components no fuctionality yet

---
+++ src/components/pages/UserProfile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function UserProfile() {
+  return (
+    <div className="user-info-container">
+      <h3>User Role</h3>
+      <h4>First Name Last Name</h4>
+
+      <p>Email</p>
+      <p>Phone Number</p>
+    </div>
+  );
+}
+
+export default UserProfile;
