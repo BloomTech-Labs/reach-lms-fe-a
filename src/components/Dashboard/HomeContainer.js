222a2b55b789496f3eea0060676f4ffac4a6cac8
Alden Ho <aldenho@Aldens-Mac-mini.local>
2021-02-16 16:24:39 -0700

    added dispatch(save user) to HomeContainer.js which will add userInfo to redux state

--- src/components/Dashboard/HomeContainer.js
+++ src/components/Dashboard/HomeContainer.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 import Dashboard from './Dashboard';
+import { useDispatch } from 'react-redux';
+import { saveUser } from '../../state/actions/userActions';
 
 function HomeContainer({ LoadingComponent }) {
   const { authState, authService } = useOktaAuth();
   const [userInfo, setUserInfo] = useState(null);
   // eslint-disable-next-line
   const [memoAuthService] = useMemo(() => [authService], []);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     let isSubscribed = true;
@@ -17,7 +20,15 @@ function HomeContainer({ LoadingComponent }) {
         // if user is authenticated we can use the authService to snag some user info.
         // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
         if (isSubscribed) {
+          const user = {
+            id: info.id,
+            fname: info.fname,
+            lname: info.lname,
+            email: info.email,
+            phone: info.phone,
+          };
           setUserInfo(info);
+          dispatch(saveUser(user));
         }
       })
       .catch(err => {
