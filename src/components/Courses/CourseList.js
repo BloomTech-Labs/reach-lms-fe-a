48ab4a5860c37ef17622583118f4dca4f3c0cb68
Alden Ho <aldenho@Aldens-Mac-mini.local>
2021-02-16 15:49:34 -0700

    created Dashboard with Program/Course List depending on user role

---
+++ src/components/Courses/CourseList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const CourseList = () => {
+  const courses = useSelector(state => state.courseReducer.courses_list);
+
+  return (
+    <div>
+      <div>
+        <h2>My Courses</h2>
+      </div>
+      <div>
+        {courses.map(course => {
+          {
+            /* return <CourseCard key={course.id} info={course} /> */
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CourseList;
---
+++ src/components/Program/ProgramList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const ProgramList = () => {
+  const programs = useSelector(state => state.programReducer.programs_list);
+
+  return (
+    <div>
+      <div>
+        <h2>My Programs</h2>
+        <Link to="/add-program">Add Program</Link>
+        Add Program
+      </div>
+      <div>
+        {programs.map(program => {
+          {
+            /* return <ProgramCard key={program.id} info={program} /> */
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ProgramList;
