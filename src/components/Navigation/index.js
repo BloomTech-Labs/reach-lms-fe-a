219481d4e43082fdb20fe537511d3bce187983b5
Alden Ho <aldenho@Aldens-Mac-mini.local>
2021-02-18 00:02:31 -0700

    fixed hamburger menu to display menu items over menu icon while active; added styling to menu items

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,13 +3,11 @@ import { Link } from 'react-router-dom';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import '../../styles/Nav.css';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,8 +24,18 @@ const useStyles = makeStyles(theme => ({
 const Navigation = props => {
   const classes = useStyles();
   let isMobile = useMediaQuery('(max-width: 800px)');
-  let [mobileMenuExpanded, setMobileMenuExpanded] = useState(false);
   const { authService } = props;
+
+  // material ui menu
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <nav>
@@ -45,64 +53,35 @@ const Navigation = props => {
             </Link>
           </div>
         )}
+        {isMobile && (
+          <>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon onClick={handleClick} />
+            </IconButton>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <Link to="/profile">
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+              </Link>
+              <Link onClick={() => authService.logout()}>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Link>
+            </Menu>
+          </>
+        )}
       </div>
-      {isMobile && (
-        <>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon
-              onClick={() => {
-                setMobileMenuExpanded(!mobileMenuExpanded);
-              }}
-            />
-          </IconButton>
-          {/* <Hamburger
-            isActive={mobileMenuExpanded}
-            onClick={() => {
-              setMobileMenuExpanded(!mobileMenuExpanded);
-            }}
-            theme={theme}
-          /> */}
-          {mobileMenuExpanded && (
-            <div>
-              <ul>
-                <li>
-                  <Link href="/profile">
-                    <a>Profile</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/logout">
-                    <a onClick={() => authService.logout()}>Logout</a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          )}
-        </>
-      )}
     </nav>
   );
 };
-
-// function DesktopView() {
-//     const { userInfo, authService } = props;
-
-//     return (
-//       <div>
-//         <Link to="/profile">
-//           <a>Profile</a>
-//         </Link>
-//         <Link href="/logout">
-//                 <a onClick={() => authService.logout()}>Logout</a>
-//         </Link>
-//       </div>
-//     );
-
-// }
 
 export default Navigation;
