a282df3ea95a953530951246a26ea08740828bc9
Alden Ho <aldenho@Aldens-Mac-mini.local>
2021-02-19 21:56:03 -0700

    createProgram Form working

--- src/components/Program/CreateProgramForm.js
+++ src/components/Program/CreateProgramForm.js
@@ -1,11 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { axiosWithAuth } from '../../utils/axiosWithAuth';
 import { useHistory } from 'react-router-dom';
-import { useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import * as yup from 'yup';
-import schema from '../../validation/Schema'
-
-import { addProgram } from "../../state/actions/programActions";
+import schema from '../../validation/Schema';
+import { addProgram } from '../../state/actions/programActions';
 
 // ant design
 import 'antd/dist/antd.css';
@@ -15,23 +14,23 @@ import Select from 'antd/lib/select';
 import Form from 'antd/lib/form/Form';
 import FormItem from 'antd/lib/form/FormItem';
 const { Option } = Select;
+const { TextArea } = Input;
 
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
 };
 
+const initialValues = {
+  programname: '',
+  programtype: '',
+  programdescription: '',
+};
 
-initialValues = {
-    name: '',
-    type: '',
-    description: '',
-}
-
-initialFormErrors = {
-    name: '',
-    type: '',
-}
+const initialFormErrors = {
+  name: '',
+  type: '',
+};
 
 export default function CreateClass() {
   const { push } = useHistory();
@@ -41,10 +40,10 @@ export default function CreateClass() {
   const user = useSelector(state => state.userReducer);
 
   if (!user.role) {
-    push("/");
+    push('/');
   }
-  
-  function changeValues(e) {
+
+  const changeValues = e => {
     e.persist();
     const correctValue = e.target.value;
 
@@ -52,120 +51,115 @@ export default function CreateClass() {
       yup
         .reach(schema, e.target.name)
         .validate(correctValue)
-        .then((res) => {
+        .then(res => {
           // console.log(res);
-          setErrors({ ...errors, [e.target.name]: '' })
+          setErrors({ ...errors, [e.target.name]: '' });
         })
-        .catch((err) => {
-          setErrors({ ...errors, [e.target.name]: err.message })
-        })
-
+        .catch(err => {
+          setErrors({ ...errors, [e.target.name]: err.message });
+        });
     }
     validate();
-    setValues({ ...values, [e.target.name]: correctValue })
-  }
+    setValues({ ...values, [e.target.name]: correctValue });
+    console.log(values);
+  };
+
+  const changeSelect = e => {
+    setValues({ ...values, programtype: e });
+  };
 
   function submitForm(e) {
     e.preventDefault();
-    dispatch(addProgram(values))
+    dispatch(addProgram(values));
+    console.log(values);
     axiosWithAuth()
-      .post('', values)
-      .then((res) => {
+      .post('https://reach-team-a-be.herokuapp.com/programs', values)
+      .then(res => {
         // console.log({createClass: res})
         //localStorage.setItem("onboarding", "true");
+        console.log(res);
         setValues(initialValues);
-        push("/dashboard");
+        push('/');
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
-      })
+      });
   }
-    
-
-      
-
-
-
 
   return (
     <div className="container">
-      <h1>Edit Program</h1>
-      <Form
-        {...layout}
-          name="basic"
-          initialValues={{
-            name: input.name,
-            type: input.type,
-            option: input.option,
-            description: input.description,
-          }}
-          onFinish={submitForm}
+      <h1>Create Program</h1>
+      <Form {...layout} name="basic" onFinish={submitForm}>
+        <FormItem
+          label="Name:"
+          name="programname"
+          rules={[
+            { required: true, message: 'Please input your program name!' },
+          ]}
         >
-          <FormItem
-            label="Name:"
-            name="name"
-            rules={[
-              { required: true, message: 'Please input your program name!' },
-            ]}
-          >
-            <Input
-              id="name"
-              name="name"
-              value={values.name}
-              onChange={changeValues}
-             />
-          </FormItem>
-    
-          <FormItem label="Type:" name="type" rules={[{ required: true }]}>
-            <Select
-              placeholder="Select a program type"
-              onChange={changeValues}
-              value={values.type}
-            >
-              <Option value="">- Select A Type -</Option>
-              <Option value="1st">-1st Grade-</Option>
-              <Option value="2nd">-2nd Grade-</Option>
-              <Option value="3rd">-3rd Grade-</Option>
-              <Option value="4th">-4th Grade-</Option>
-              <Option value="5th">-5th Grade-</Option>
-              <Option value="6th">-6th Grade-</Option>
-              <Option value="7th">-7th Grade-</Option>
-              <Option value="8th">-8th Grade-</Option>                
-              <Option value="9th">-9th Grade-</Option>
-              <Option value="10th">-10th Grade-</Option>
-              <Option value="11th">-11th Grade-</Option>
-              <Option value="12th">-12th Grade-</Option>
-              <Option value="higher">-Higher-</Option>
-              <Option value="training">-Training-</Option>
-              <Option value="other">-Other-</Option>
-            </Select>
-          </FormItem>
-    
-          <FormItem
-            label="Description:"
-            name="description"
-            rules={[
-              {
-                required: false,
-                message: 'Please add a description for your program!',
-              },
-            ]}
-          >
-            <Input
-              id="description"
-              name="description"
-              value={values.description}
-              onChange={changeValues}
-            />
-          </FormItem>
-            <Button type="primary">Submit</Button>
-          </Form>
-        </div>
-      );
-          }
-     
+          <Input
+            id="programname"
+            name="programname"
+            value={values.name}
+            onChange={changeValues}
+          />
+        </FormItem>
 
-  
-          
-  
-    
+        <FormItem label="Type:" name="programtype" rules={[{ required: true }]}>
+          <Select
+            id="programtype"
+            name="programtype"
+            value={values.programtype}
+            placeholder="Select a program type"
+            onChange={changeSelect}
+          >
+            <Option value="">- Select A Type -</Option>
+            <Option value="1st">-1st Grade-</Option>
+            <Option value="2nd">-2nd Grade-</Option>
+            <Option value="3rd">-3rd Grade-</Option>
+            <Option value="4th">-4th Grade-</Option>
+            <Option value="5th">-5th Grade-</Option>
+            <Option value="6th">-6th Grade-</Option>
+            <Option value="7th">-7th Grade-</Option>
+            <Option value="8th">-8th Grade-</Option>
+            <Option value="9th">-9th Grade-</Option>
+            <Option value="10th">-10th Grade-</Option>
+            <Option value="11th">-11th Grade-</Option>
+            <Option value="12th">-12th Grade-</Option>
+            <Option value="higher">-Higher-</Option>
+            <Option value="training">-Training-</Option>
+            <Option value="other">-Other-</Option>
+          </Select>
+        </FormItem>
+        <FormItem
+          label="Description:"
+          name="programdescription"
+          rules={[
+            {
+              required: true,
+              message: 'Please add a description for your program!',
+            },
+          ]}
+        >
+          {/* <Input
+            id="programdescription"
+            name="programdescription"
+            value={values.programdescription}
+            onChange={changeValues}
+          /> */}
+          <TextArea
+            showCount
+            maxLength={1000}
+            id="programdescription"
+            name="programdescription"
+            value={values.programdescription}
+            onChange={changeValues}
+          />
+        </FormItem>
+        <Button onClick={submitForm} type="primary">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
