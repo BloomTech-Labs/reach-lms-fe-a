f701644bd1d126490d2b77429a16278cfe7b69fb
Alden Ho <aldenho@Aldens-Mac-mini.local>
2021-02-22 10:34:57 -0700

    fixes small console errors on navigation and dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,11 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { axiosWithAuth } from '../../utils/axiosWithAuth';
 import { setProgramList } from '../../state/actions/programActions';
 import Navigation from '../Navigation';
-import { addProgram } from '../../state/actions/programActions';
 import ProgramList from '../Program/ProgramList';
 import CourseList from '../Courses/CourseList';
 // ant Design
-import { saveUser } from '../../state/actions/userActions';
 import { Link } from 'react-router-dom';
 const Dashboard = props => {
   const { userInfo, authService } = props;
@@ -24,41 +22,6 @@ const Dashboard = props => {
       .catch(err => {
         console.log(err);
       });
-
-    // axiosWithAuth()
-    //   .get(``)
-    //   .then(res => {
-    //     console.log(res)
-    //   })
-    //   .catch(err => {
-    //     console.log(err)
-    //   })
-
-    // async function fetchData() {
-    //   if (userRole === 'ADMIN') {
-    //     let doneLoading = await axiosWithAuth()
-    //       .get(`https://reach-team-a-be.herokuapp.com/${user.id}`)
-    //       .then(res => {
-    //         console.log(res)
-    //         return false;
-    //       })
-    //       .catch(err => console.log(err));
-    //   } else {
-    //     let doneLoading = await axiosWithAuth()
-    //       .get(``)
-    //       .then(res => {
-    //         dispatch(setProgramList(res.data.data));
-    //         return false;
-    //       })
-    //       .catch(err => console.log(err));
-    //   }
-    // }
-
-    // if (isMounted) {
-    //   fetchData(userInfo);
-    // }
-
-    // return () => (isMounted = false);
   }, []);
 
   return (
