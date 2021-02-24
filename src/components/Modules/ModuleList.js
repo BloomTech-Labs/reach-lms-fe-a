889a936f30e9ff45b56ec1fc2101813f842ed1cb
Simone Ballard <simone.a.ballard@gmail.com>
2021-02-23 19:04:35 -0700

    created module list and added preliminary ant design

--- src/components/Modules/ModuleList.js
+++ src/components/Modules/ModuleList.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import ModuleCard from './ModuleCard';
+//ant d
+import { Layout } from 'antd';
+
+//ant Design
+const { Header, Footer, Content } = Layout;
 
 const ModuleList = props => {
   const modules = useSelector(state => state.moduleReducer.modules_list);
 
   return (
-    <div>
-      <div>
+    <Layout>
+      <Header>
         <h2>My Modules</h2>
-      </div>
-      <div>
+      </Header>
+      <Content>
         {modules.map(module => {
           return <ModuleCard key={module.moduleid} module={module} />;
         })}
-      </div>
-    </div>
+      </Content>
+      <Footer></Footer>
+    </Layout>
   );
 };
 
