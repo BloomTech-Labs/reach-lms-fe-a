3128a40c24aa2604155a4ded1c32513f77848382
Simone Ballard <simone.a.ballard@gmail.com>
2021-02-23 19:30:12 -0700

    commit components

--- src/components/Modules/ModuleCard.js
+++ src/components/Modules/ModuleCard.js
@@ -5,7 +5,6 @@
 // import { useSelector, useDispatch } from 'react-redux';
 // import { useHistory } from 'react-router-dom';
 // import { axiosWithAuth } from '../../utils/axiosWithAuth';
-// import { setEditModule, deleteModule } from '../../state/actions/moduleActions';
 
 // export default function ModuleCard(props) {
 //   const { module } = props;
@@ -13,72 +12,26 @@
 //   const dispatch = useDispatch();
 //   const { push } = useHistory();
 
-//   const handleMenuClick = e => {
-//     if (e.key === 'edit') {
-//     //   dispatch(setEditModule(module));
-//     //   push('/edit-module');
-//     } else {
-//       axiosWithAuth()
-//         .delete(
-//         //   `https://reach-team-a-be.herokuapp.com/modules/${module.moduleid}`
-//         )
-//         .then(res => {
-//           console.log(module.moduleid);
-//           dispatch(deleteModule(module.moduleid));
-//           console.log(res);
-//         })
-//         .catch(err => console.log(err));
-//     }
-//   };
-
-//   const menu = (
-//     <Menu onClick={handleMenuClick}>
-//       {/* <Menu.Item key="edit">Edit Module</Menu.Item> */}
-//       <Menu.Item key="delete">Delete Module</Menu.Item>
-//     </Menu>
-//   );
-
-//   function clickOnEdit(e, id) {
-//     console.log('module', module);
-//     dispatch(setEditModule(module));
-//     push('/edit-module');
-//   }
-
-//   function deletingModule(id) {
-//     console.log(module);
-//     axiosWithAuth()
-//       // will have to put in the proper API call here
-//       .delete(``)
-//       .then(res => console.log(res))
-//       .catch(err => console.log(err));
-
-//     dispatch(deleteModule(id));
-//   }
-
 //   const viewModuleHandler = id => {
 //     axiosWithAuth()
-//       .get(`https://reach-team-a-be.herokuapp.com/modules/${id}`)
+//       .get(``)
 //       .then(res => {
 //         console.log(res);
 //         console.log(id);
-//         // dispatch(setModuleList(res.data));
-//         // dispatch(viewProgram(id));
+//         dispatch(setModuleList(res.data));
+//         dispatch(viewModule(id));
 //       })
 //       .catch(err => console.log(err));
 //     push('/module-text'); //this should be the eventual route
 //   };
 
-// module has name, description, content
+// //module has name, description, content, but in the dropdown only the name will show ...? need to discuss
 
 //   return (
 //     <>
 //       <Card
 //         title={module.modulename}
-//         extra={<Dropdown.Button overlay={menu}></Dropdown.Button>}
-//         style={{ width: 800 }}
 //       >
-//         <h3>{module.moduledescription}</h3>
-//         <p>{module.modulecontent}</p>
 //         <Button
 //           onClick={() => viewModuleHandler(module.moduleid)}
 //           type="primary"
--- src/components/Modules/ModuleList.js
+++ src/components/Modules/ModuleList.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import ModuleCard from './ModuleCard';
-//ant d
-import { Layout } from 'antd';
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+// import ModuleCard from './ModuleCard';
+// //ant d
+// import { Layout } from 'antd';
 
-//ant Design
-const { Header, Footer, Content } = Layout;
+// //ant Design
+// const { Header, Footer, Content } = Layout;
 
-const ModuleList = props => {
-  const modules = useSelector(state => state.moduleReducer.modules_list);
+// const ModuleList = props => {
+//   const modules = useSelector(state => state.moduleReducer.modules_list);
 
-  return (
-    <Layout>
-      <Header>
-        <h2>My Modules</h2>
-      </Header>
-      <Content>
-        {modules.map(module => {
-          return <ModuleCard key={module.moduleid} module={module} />;
-        })}
-      </Content>
-      <Footer></Footer>
-    </Layout>
-  );
-};
+//   return (
+//     <Layout>
+//       <Header>
+//         <h2>My Modules</h2>
+//       </Header>
+//       <Content>
+//         {modules.map(module => {
+//           return <ModuleCard key={module.moduleid} module={module} />;
+//         })}
+//       </Content>
+//       <Footer></Footer>
+//     </Layout>
+//   );
+// };
 
-export default ModuleList;
+// export default ModuleList;
