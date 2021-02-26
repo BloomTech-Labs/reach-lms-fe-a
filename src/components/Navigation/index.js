3dd9d0ce9509690c31f15bbf0e2bec0d881fa9d1
Alden Ho <aldenho@Aldens-Mac-mini.local>
2021-02-25 18:55:51 -0700

    changed import to refix hamburger menu

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
