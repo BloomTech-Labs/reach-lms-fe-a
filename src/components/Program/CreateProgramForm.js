1b80cd85fa02084b6faa012f1ebbc6602899791e
Simone Ballard <simone.a.ballard@gmail.com>
2021-03-02 16:17:58 -0700

    Create program form styling updated

--- src/components/Program/CreateProgramForm.js
+++ src/components/Program/CreateProgramForm.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from '../../utils/axiosWithAuth';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import Navigation from '../Navigation';
 import * as yup from 'yup';
 import schema from '../../validation/ProgramSchema';
 import { addProgram } from '../../state/actions/programActions';
+import styled from 'styled-components';
 
 // css
 import '../../styles/Form.css';
-
 // ant design
 import 'antd/dist/antd.css';
 import Button from 'antd/lib/button';
@@ -16,8 +17,18 @@ import Input from 'antd/lib/input';
 import Select from 'antd/lib/select';
 import Form from 'antd/lib/form/Form';
 import FormItem from 'antd/lib/form/FormItem';
+import { Layout } from 'antd';
 const { Option } = Select;
 const { TextArea } = Input;
+const { Header, Footer, Content } = Layout;
+
+//styled components
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 3%;
+  margin-left: 10%;
+`;
 
 const layout = {
   labelCol: { span: 8 },
@@ -92,112 +103,126 @@ export default function CreateProgram() {
   };
 
   return (
-    <div className="container">
-      <h1 className="edit-form-h1">Create Program</h1>
-      <Form {...layout} name="basic" onFinish={submitForm} className="form">
-        <FormItem htmlFor="programname" label="Program Name:" validateStatus>
-          <Input
-            id="programname"
-            name="programname"
-            value={values.name}
-            onChange={changeValues}
-          />
-          <div style={{ color: 'red' }}>
-            {errors.programname ? `${errors.programname}` : ''}
-          </div>
-        </FormItem>
+    <Layout>
+      <Header>
+        <Navigation />
+      </Header>
+      <StyledContainer>
+        <Content>
+          <h1 className="edit-form-h1">Create Program</h1>
+          <Form {...layout} name="basic" onFinish={submitForm} className="form">
+            <FormItem
+              htmlFor="programname"
+              label="Program Name:"
+              validateStatus
+            >
+              <Input
+                id="programname"
+                name="programname"
+                value={values.name}
+                onChange={changeValues}
+              />
+              <div style={{ color: 'red' }}>
+                {errors.programname ? `${errors.programname}` : ''}
+              </div>
+            </FormItem>
 
-        <FormItem htmlFor="programtype" label="Program Type:">
-          <Select
-            id="programtype"
-            name="programtype"
-            value={values.programtype}
-            placeholder="Select a program type"
-            onSelect={(value, event) => changeSelect(value, event)}
-            data-testid="select"
-          >
-            <Option data-testid="select-option" value="">
-              - Select A Type -
-            </Option>
-            <Option data-testid="select-option" value="1st Grade">
-              -1st Grade-
-            </Option>
-            <Option data-testid="select-option" value="2nd Grade">
-              -2nd Grade-
-            </Option>
-            <Option data-testid="select-option" value="3rd Grade">
-              -3rd Grade-
-            </Option>
-            <Option data-testid="select-option" value="4th Grade">
-              -4th Grade-
-            </Option>
-            <Option data-testid="select-option" value="5th Grade">
-              -5th Grade-
-            </Option>
-            <Option data-testid="select-option" value="6th Grade">
-              -6th Grade-
-            </Option>
-            <Option data-testid="select-option" value="7th Grade">
-              -7th Grade-
-            </Option>
-            <Option data-testid="select-option" value="8th Grade">
-              -8th Grade-
-            </Option>
-            <Option data-testid="select-option" value="9th Grade">
-              -9th Grade-
-            </Option>
-            <Option data-testid="select-option" value="10th Grade">
-              -10th Grade-
-            </Option>
-            <Option data-testid="select-option" value="11th Grade">
-              -11th Grade-
-            </Option>
-            <Option data-testid="select-option" value="12th Grade">
-              -12th Grade-
-            </Option>
-            <Option data-testid="select-option" value="Higher">
-              -Higher-
-            </Option>
-            <Option data-testid="select-option" value="Training">
-              -Training-
-            </Option>
-            <Option data-testid="select-option" value="Other">
-              -Other-
-            </Option>
-          </Select>
-          <div style={{ color: 'red' }}>
-            {errors.programtype ? `${errors.programtype}` : ''}
-          </div>
-        </FormItem>
+            <FormItem htmlFor="programtype" label="Program Type:">
+              <Select
+                id="programtype"
+                name="programtype"
+                value={values.programtype}
+                placeholder="Select a program type"
+                onSelect={(value, event) => changeSelect(value, event)}
+                data-testid="select"
+              >
+                <Option data-testid="select-option" value="">
+                  - Select A Type -
+                </Option>
+                <Option data-testid="select-option" value="1st Grade">
+                  -1st Grade-
+                </Option>
+                <Option data-testid="select-option" value="2nd Grade">
+                  -2nd Grade-
+                </Option>
+                <Option data-testid="select-option" value="3rd Grade">
+                  -3rd Grade-
+                </Option>
+                <Option data-testid="select-option" value="4th Grade">
+                  -4th Grade-
+                </Option>
+                <Option data-testid="select-option" value="5th Grade">
+                  -5th Grade-
+                </Option>
+                <Option data-testid="select-option" value="6th Grade">
+                  -6th Grade-
+                </Option>
+                <Option data-testid="select-option" value="7th Grade">
+                  -7th Grade-
+                </Option>
+                <Option data-testid="select-option" value="8th Grade">
+                  -8th Grade-
+                </Option>
+                <Option data-testid="select-option" value="9th Grade">
+                  -9th Grade-
+                </Option>
+                <Option data-testid="select-option" value="10th Grade">
+                  -10th Grade-
+                </Option>
+                <Option data-testid="select-option" value="11th Grade">
+                  -11th Grade-
+                </Option>
+                <Option data-testid="select-option" value="12th Grade">
+                  -12th Grade-
+                </Option>
+                <Option data-testid="select-option" value="Higher">
+                  -Higher-
+                </Option>
+                <Option data-testid="select-option" value="Training">
+                  -Training-
+                </Option>
+                <Option data-testid="select-option" value="Other">
+                  -Other-
+                </Option>
+              </Select>
+              <div style={{ color: 'red' }}>
+                {errors.programtype ? `${errors.programtype}` : ''}
+              </div>
+            </FormItem>
 
-        <FormItem htmlFor="programdescription" label="Program Description:">
-          <TextArea
-            showCount
-            maxLength={500}
-            id="programdescription"
-            name="programdescription"
-            value={values.programdescription}
-            onChange={changeValues}
-            rows={4}
-          />
-          <div style={{ color: 'red' }}>
-            {errors.programdescription ? `${errors.programdescription}` : ''}
-          </div>
-        </FormItem>
-        <div className="button-container">
-          <Button onClick={goBack} type="secondary" className="button">
-            Cancel
-          </Button>
-          <Button
-            onClick={submitForm}
-            type="primary"
-            disabled={disabled}
-            className="button"
-          >
-            Submit
-          </Button>
-        </div>
-      </Form>
-    </div>
+            <FormItem htmlFor="programdescription" label="Program Description:">
+              <TextArea
+                showCount
+                maxLength={500}
+                id="programdescription"
+                name="programdescription"
+                value={values.programdescription}
+                onChange={changeValues}
+                rows={4}
+              />
+              <div style={{ color: 'red' }}>
+                {errors.programdescription
+                  ? `${errors.programdescription}`
+                  : ''}
+              </div>
+            </FormItem>
+            <div className="button-container">
+              <Button onClick={goBack} type="secondary" className="button">
+                Cancel
+              </Button>
+              <Button
+                onClick={submitForm}
+                type="primary"
+                disabled={disabled}
+                className="button"
+              >
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </Content>
+      </StyledContainer>
+      <Footer></Footer>
+    </Layout>
   );
 }
