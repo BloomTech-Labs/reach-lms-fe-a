48d87cac591883a82f672c24ad4abf1b3a3435fd
Alden Ho <aldenho@Aldens-Mac-mini.local>
2021-03-01 17:17:35 -0700

    adds responsive design to courses page

--- src/components/Courses/CourseList.js
+++ src/components/Courses/CourseList.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import CourseCard from './CourseCard';
-import SearchPage from '../Search/SearchPage';
 
 //ant d
 import 'antd/dist/antd.css';
@@ -20,17 +19,6 @@ const StyledCourses = styled.div`
   justify-content: center;
 `;
 
-const HeaderDiv = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-  margin: 5% 0;
-  @media (max-width: 450px) {
-    flex-direction: column;
-  }
-`;
-
 const StyledContent = styled.div`
   width: 100%;
   display: flex;
@@ -42,6 +30,42 @@ const StyledWrapper = styled.div`
   display: flex;
   margin-left: 10%;
   width: 50%;
+`;
+
+const StyledTitle = styled.div`
+  display: flex;
+  margin-left: 10%;
+  @media (max-width: 450px) {
+    margin: 5% 0;
+    display: flex;
+    justify-content: center;
+    margin-left: 0%;
+  }
+`;
+
+const HeaderDiv = styled.div`
+  display: flex;
+  width: 800px;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5% 0;
+  @media (max-width: 885px) {
+    width: 600px;
+  }
+  @media (max-width: 665px) {
+    width: 400px;
+  }
+  @media (max-width: 450px) {
+    width: 325px;
+    flex-direction: column;
+  }
+  @media (max-width: 350px) {
+    width: 275px;
+  }
+`;
+
+const StyledH2 = styled.h2`
+  font-size: 1.75rem;
 `;
 
 const CourseList = props => {
@@ -69,14 +93,16 @@ const CourseList = props => {
                 )}
               </div>
               <HeaderDiv>
-                <h2>My Courses</h2>
-                {user.role === 'ADMIN' && (
-                  <Link to="/add-course">
-                    <Button size="large" style={{ background: '#01fe87' }}>
-                      Add Course
-                    </Button>
-                  </Link>
-                )}
+                <StyledH2>My Courses</StyledH2>
+                <StyledTitle>
+                  {user.role === 'ADMIN' && (
+                    <Link to="/add-course">
+                      <Button size="large" style={{ background: '#01fe87' }}>
+                        Add Course
+                      </Button>
+                    </Link>
+                  )}
+                </StyledTitle>
               </HeaderDiv>
               <StyledCourses>
                 {courseList.map(course => {
