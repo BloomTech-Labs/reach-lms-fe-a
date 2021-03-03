908cdbb5f71537849728549b6c112bae7b7d4021
Simone Ballard <simone.a.ballard@gmail.com>
2021-03-03 15:34:43 -0700

    Styled edit user form

--- src/components/Profile/EditUserForm.js
+++ src/components/Profile/EditUserForm.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { axiosWithAuth } from '../../utils/axiosWithAuth';
+import Navigation from '../Navigation';
 import schema from '../../validation/ProfileSchema';
 import * as yup from 'yup';
 import { editUser } from '../../state/actions/userActions';
-
+import styled from 'styled-components';
 // css
 import '../../styles/Form.css';
 
@@ -15,6 +16,16 @@ import Button from 'antd/lib/button';
 import Input from 'antd/lib/input';
 import Form from 'antd/lib/form/Form';
 import FormItem from 'antd/lib/form/FormItem';
+import { Layout } from 'antd';
+const { Header, Footer, Content } = Layout;
+
+//styled components
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 3%;
+  margin-left: 10%;
+`;
 
 const layout = {
   labelCol: { span: 7 },
@@ -83,78 +94,86 @@ export default function EditUserForm() {
   };
 
   return (
-    <div className="container">
-      <h1 className="edit-form-h1">Edit User</h1>
-      <Form
-        {...layout}
-        name="basic"
-        onFinish={editUserSubmit}
-        initialValues={{
-          firstname: user.firstname,
-          lastname: user.lastname,
-          phonenumber: user.phonenumber,
-          email: user.email,
-        }}
-        className="form"
-      >
-        <FormItem htmlFor="firstname" label="First Name:">
-          <Input
-            id="firstname"
-            name="firstname"
-            value={input.firstname}
-            onChange={changeValues}
-          />
-          <div style={{ color: 'red' }}>
-            {errors.firstname ? `${errors.firstname}` : ''}
-          </div>
-        </FormItem>
-        <FormItem htmlFor="lastname" label="Last Name:">
-          <Input
-            id="lastname"
-            name="lastname"
-            value={input.lastname}
-            onChange={changeValues}
-          />
-          <div style={{ color: 'red' }}>
-            {errors.lastname ? `${errors.lastname}` : ''}
-          </div>
-        </FormItem>
-        <FormItem htmlFor="email" label="Email:" name="email">
-          <Input
-            id="email"
-            name="email"
-            value={input.email}
-            onChange={changeValues}
-          />
-          <div style={{ color: 'red' }}>
-            {errors.email ? `${errors.email}` : ''}
-          </div>
-        </FormItem>
-        <FormItem htmlFor="phonenumber" label="Phone Number:">
-          <Input
-            id="phonenumber"
-            name="phonenumber"
-            value={input.phonenumber}
-            onChange={changeValues}
-          />
-          <div style={{ color: 'red' }}>
-            {errors.phonenumber ? `${errors.phonenumber}` : ''}
-          </div>
-        </FormItem>
-        <div className="button-container">
-          <Button onClick={goBack} type="secondary" className="button">
-            Cancel
-          </Button>
-          <Button
-            onClick={editUserSubmit}
-            type="primary"
-            disabled={disabled}
-            className="button"
+    <Layout>
+      <Header>
+        <Navigation />
+      </Header>
+      <StyledContainer>
+        <Content>
+          <h1 className="edit-form-h1">Edit User</h1>
+          <Form
+            {...layout}
+            name="basic"
+            onFinish={editUserSubmit}
+            initialValues={{
+              firstname: user.firstname,
+              lastname: user.lastname,
+              phonenumber: user.phonenumber,
+              email: user.email,
+            }}
+            className="form"
           >
-            Submit
-          </Button>
-        </div>
-      </Form>
-    </div>
+            <FormItem htmlFor="firstname" label="First Name:">
+              <Input
+                id="firstname"
+                name="firstname"
+                value={input.firstname}
+                onChange={changeValues}
+              />
+              <div style={{ color: 'red' }}>
+                {errors.firstname ? `${errors.firstname}` : ''}
+              </div>
+            </FormItem>
+            <FormItem htmlFor="lastname" label="Last Name:">
+              <Input
+                id="lastname"
+                name="lastname"
+                value={input.lastname}
+                onChange={changeValues}
+              />
+              <div style={{ color: 'red' }}>
+                {errors.lastname ? `${errors.lastname}` : ''}
+              </div>
+            </FormItem>
+            <FormItem htmlFor="email" label="Email:" name="email">
+              <Input
+                id="email"
+                name="email"
+                value={input.email}
+                onChange={changeValues}
+              />
+              <div style={{ color: 'red' }}>
+                {errors.email ? `${errors.email}` : ''}
+              </div>
+            </FormItem>
+            <FormItem htmlFor="phonenumber" label="Phone Number:">
+              <Input
+                id="phonenumber"
+                name="phonenumber"
+                value={input.phonenumber}
+                onChange={changeValues}
+              />
+              <div style={{ color: 'red' }}>
+                {errors.phonenumber ? `${errors.phonenumber}` : ''}
+              </div>
+            </FormItem>
+            <div className="button-container">
+              <Button onClick={goBack} type="secondary" className="button">
+                Cancel
+              </Button>
+              <Button
+                onClick={editUserSubmit}
+                type="primary"
+                disabled={disabled}
+                className="button"
+              >
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </Content>
+      </StyledContainer>
+      <Footer></Footer>
+    </Layout>
   );
 }
