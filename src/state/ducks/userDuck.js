f6ac5671146dee3ea8f0b650462cdb575e24ed14
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-03-09 21:41:21 -0600

    clean up userDuck

--- src/state/ducks/userDuck.js
+++ src/state/ducks/userDuck.js
@@ -46,7 +46,10 @@ const userReducer = (state = initialState, action) => {
 
   switch (action.type) {
     case SAVE_USER:
-      return action.payload;
+      return {
+        ...state,
+        user: action.payload,
+      };
     case CLEAR_USER:
       return initialState;
     case EDIT_USER:
