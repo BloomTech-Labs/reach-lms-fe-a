f658f9146432e431538e377cd436d2ee4f2fdc4d
Foleso <foleso.guard@gmail.com>
2021-03-16 00:23:16 -0400

    Added some functionality

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import UserCard from './UserCard';
 
 export default function UserDashboard() {
-  return <div>User Dashboard goes here</div>;
+  return (
+    <div>
+      User Dashboard goes here
+      <UserCard></UserCard>
+    </div>
+  );
 }
