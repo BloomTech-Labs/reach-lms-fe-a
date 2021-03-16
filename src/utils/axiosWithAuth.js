193010a7c609ab39039af890f6d367b0c1657744
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-03-15 21:30:53 -0400

    remove Access-Allow-Origin-* in axiosAuth headers

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -7,8 +7,6 @@ const getBaseParams = () => {
   return {
     headers: {
       Authorization: `Bearer ${access_token}`,
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     },
   };
 };
