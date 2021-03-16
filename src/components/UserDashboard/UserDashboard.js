6738b82ac8ba7f4706ed33f6762589b7cf1311ea
Foleso <foleso.guard@gmail.com>
2021-03-16 01:09:30 -0400

    Added Ducks for the All Users component

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { axiosWithAuth } from '../../utils/axiosWithAuth';
 import UserCard from './UserCard';
 
-export default function UserDashboard() {
+const UserDashboard = () => {
   return (
     <div>
       User Dashboard goes here
       <UserCard></UserCard>
     </div>
   );
-}
+};
+
+export default UserDashboard;
