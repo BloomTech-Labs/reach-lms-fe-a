fcac63327267adedc92a258f48f62498e33571cc
Sebastian <sebastianmendoza482@gmail.com>
2021-03-16 19:35:45 -0700

    commented selected.allUsers

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -7,6 +7,7 @@ import { axiosWithAuth } from '../../utils/axiosWithAuth';
 import UserCard from './UserCard';
 
 import { userActions } from '../../state/ducks';
+import { ContactSupportOutlined } from '@material-ui/icons';
 
 const UserDashboard = () => {
   const selected = useSelector(state => state.userReducer);
@@ -19,11 +20,12 @@ const UserDashboard = () => {
   return (
     <div>
       <h2>ID, Username, First Name, Last Name, Role </h2>
-      {selected.allUsers == undefined && <div>Loading...</div>}
+      {/* {selected.allUsers == undefined && <div>Loading...</div>}
       {selected.allUsers != undefined &&
         selected.allUsers.map(item => (
           <UserCard key={item.userid} user={item}></UserCard>
-        ))}
+        ))} */}
+      {/* ^^^^^^^^^^^^^^This code breaks, needs to be fixed in userDuck ^^^^^^^^^^^^^^  */}
     </div>
   );
 };
