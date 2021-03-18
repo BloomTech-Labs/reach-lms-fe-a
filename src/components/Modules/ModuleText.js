c9a267cf4c1abf3eab0159e89dff0c1f309d1020
Simone Ballard <simone.a.ballard@gmail.com>
2021-03-18 14:38:40 -0600

    ModuleText updated to incorporate all comments from yesterday

--- src/components/Modules/ModuleText.js
+++ src/components/Modules/ModuleText.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useHistory, useParams } from 'react-router-dom';
 import { moduleActions } from '../../state/ducks';
 import styled from 'styled-components';
 import { pathUtils } from '../../routes';
+import { useUserRole } from '../../hooks';
+import { MenuItemLink } from '../_common';
 
 //ant design
 import 'antd/dist/antd.css';
@@ -20,32 +21,25 @@ const StyledContainer = styled.div`
   margin-left: 10%;
 `;
 
-export default function ModuleText() {
-  const { currentModule, status } = useSelector(
-    state => state.moduleReducer.currentModule
-  );
-  const user = useSelector(state => state.userReducer);
+export default function ModuleText(props) {
+  const { status, error } = useSelector(state => state.moduleReducer);
+  const { course, courseId, module, push } = props;
   const dispatch = useDispatch();
-  const { push } = useHistory();
-  const { courseId, moduleId } = useParams();
+
+  const { userIsAdmin, userIsTeacher } = useUserRole();
+
+  const VIEW_MODULES = pathUtils.makeViewAllModulesPath(course.courseid);
 
   useEffect(() => {
-    if (status === 'delete/success') {
-      // TODO needs support for new modules path
-      // import {pathUtils} from "../../routes";
-      // use pathUtils.makeViewAllModulesPath function
-      // grab course id from use params hook
-      dispatch(moduleActions.deleteModuleThunk(module.moduleid));
-      push(pathUtils.makeViewAllModulesPath(courseId));
+    if (status === 'get-module-by-module-id/error') {
+      console.error(error);
     }
-  }, [push, status]);
+  }, [error, status]);
 
   const handleMenuClick = e => {
     if (e.key === 'edit') {
-      dispatch(moduleActions.getModuleByModuleIdThunk(module.moduleid));
-      // TODO needs support for new edit-module path — import {pathUtils} from "../../routes";
-      // use pathUtils.makeEditModulePath(moduleId)
-      push(pathUtils.makeEditModulePath(moduleId));
+      dispatch(moduleActions.setEditModule(module));
+      push(pathUtils.makeEditModulePath(courseId, module.moduleid));
     } else {
       dispatch(moduleActions.deleteModuleThunk(module.moduleid));
     }
@@ -53,34 +47,37 @@ export default function ModuleText() {
 
   const menu = (
     <Menu onClick={handleMenuClick}>
-      <Menu.Item key="edit">Edit Module</Menu.Item>
-      <Menu.Item key="delete">Delete Module</Menu.Item>
+      <MenuItemLink
+        to={pathUtils.makeEditModulePath(module.moduleid)}
+        key="edit"
+        extra=""
+      >
+        Edit Module
+      </MenuItemLink>
+      {userIsAdmin() && <Menu.Item key="delete">Delete Module</Menu.Item>}
     </Menu>
   );
 
-  const goBack = () => {
-    push('/modules');
+  const viewCourseHandler = () => {
+    push(VIEW_MODULES);
   };
 
   return (
     <StyledContainer>
       <Card
-        title={currentModule.modulename}
+        title={module.modulename}
         extra={
-          user.role === 'ADMIN' ? (
+          (userIsAdmin() || userIsTeacher()) && (
             <Dropdown.Button overlay={menu}></Dropdown.Button>
-          ) : user.role === 'TEACHER' ? (
-            <Dropdown.Button overlay={menu}></Dropdown.Button>
-          ) : (
-            <div></div>
           )
         }
+        className="module-text"
         style={{ width: 800 }}
       >
-        <h3>{currentModule.moduledescription}</h3>
-        <p>{currentModule.modulecontent}</p>
-        <Button onClick={goBack} type="secondary" className="button">
-          Return to Modules List
+        <h3>Module Description: {module.moduledescription}</h3>
+        <p>Description: {module.modulecontent}</p>
+        <Button onClick={viewCourseHandler} type="primary">
+          Return
         </Button>
       </Card>
     </StyledContainer>
