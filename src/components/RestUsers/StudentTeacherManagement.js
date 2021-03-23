c20b637d49ba7ce774891d3f7549952ce87b9961
Christopher Girvin <chrisg8691@gmail.com>
2021-03-23 00:03:34 -0400

    switching to pc

--- src/components/RestUsers/StudentTeacherManagement.js
+++ src/components/RestUsers/StudentTeacherManagement.js
@@ -2,6 +2,114 @@ import React from 'react';
 import UserList from './UserList';
 import { client } from '../../utils/api';
 import { useRestfulFetch } from '../../hooks';
+import styled from 'styled-components';
+
+const SSplit = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  .left {
+    background-color: white;
+  }
+  .right {
+    background-color: white;
+  }
+`;
+
+const SplitFrame = props => {
+  return (
+    <SSplit>
+      <div className="left">{props.left}</div>
+      <div className="right">{props.right}</div>
+    </SSplit>
+  );
+};
+
+const Teachers = ({ data, onChange }) => {
+  return (
+    <>
+      <h2>Teachers</h2>
+      <div>
+        <h3>Assigned</h3>
+        <UserList
+          href={data._links.enrolled_teachers.href}
+          mappedChild={userEntity => (
+            <>
+              <br />
+              <input
+                type="checkbox"
+                name={userEntity.userid}
+                onChange={onChange}
+              />
+              <div>{userEntity.username}</div>
+            </>
+          )}
+        />
+        <br />
+        <h3>Not Assigned</h3>
+        <UserList
+          href={data._links.available_teachers.href}
+          mappedChild={userEntity => (
+            <>
+              <br />
+              <input
+                type="checkbox"
+                name={userEntity.userid}
+                onChange={onChange}
+              />
+              <div>{userEntity.username}</div>
+            </>
+          )}
+        />
+      </div>
+    </>
+  );
+};
+
+const Students = ({ data, onChange }) => {
+  return (
+    <>
+      <h2>Students</h2>
+      <div>
+        <h3>Enrolled</h3>
+        <UserList
+          href={data._links.enrolled_students.href}
+          mappedChild={userEntity => (
+            <>
+              <br />
+              <input
+                type="checkbox"
+                checked={true}
+                name={userEntity.userid}
+                onChange={onChange}
+              />
+              <div>{userEntity.username}</div>
+            </>
+          )}
+        />
+      </div>
+      <br />
+      <div>
+        <h3>Un-Enrolled</h3>
+        <UserList
+          href={data._links.available_students.href}
+          mappedChild={userEntity => (
+            <>
+              <br />
+              <input
+                type="checkbox"
+                checked={false}
+                name={userEntity.userid}
+                onChange={onChange}
+              />
+              <div>{userEntity.username}</div>
+            </>
+          )}
+        />
+      </div>
+    </>
+  );
+};
 
 const StudentTeacherManagement = props => {
   const { data } = useRestfulFetch(props.href);
@@ -20,46 +128,10 @@ const StudentTeacherManagement = props => {
   }
 
   return (
-    <>
-      <div>
-        <div>
-          <h2>Enrolled</h2>
-          <UserList
-            href={data._links.enrolled.href}
-            mappedChild={userEntity => (
-              <>
-                <br />
-                <input
-                  type="checkbox"
-                  checked={true}
-                  name={userEntity.userid}
-                  onChange={onChange}
-                />
-                <div>{userEntity.username}</div>
-              </>
-            )}
-          />
-        </div>
-        <div>
-          <h2>Un-Enrolled</h2>
-          <UserList
-            href={data._links.not_enrolled.href}
-            mappedChild={userEntity => (
-              <>
-                <br />
-                <input
-                  type="checkbox"
-                  checked={false}
-                  name={userEntity.userid}
-                  onChange={onChange}
-                />
-                <div>{userEntity.username}</div>
-              </>
-            )}
-          />
-        </div>
-      </div>
-    </>
+    <SplitFrame
+      left={<Teachers data={data} onChange={onChange} />}
+      right={<Students data={data} onChange={onChange} />}
+    />
   );
 };
 
