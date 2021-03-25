991172401165fa561d34ebb3043d690cdfd44418
Sebastian <sebastianmendoza482@gmail.com>
2021-03-25 11:06:33 -0700

    added onClickHandler for CirclePicker on CourseCard

--- src/components/RestCourses/CourseCard.js
+++ src/components/RestCourses/CourseCard.js
@@ -2,13 +2,22 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { Card } from 'antd';
 import '../../styles/CourseCard.css';
-
+import { CirclePicker } from 'react-color';
+import { useState } from 'react';
 const CourseCard = props => {
   const { course } = props;
+  const [color, setColor] = useState('');
+  const handleChangeComplete = color => {
+    setColor(color.hex);
+    console.log(color);
+  };
   return (
     <Card
+      style={{
+        backgroundColor: color,
+      }}
       title={
-        <h3>
+        <h3 color={color}>
           <strong>{course.coursename}</strong>
         </h3>
       }
@@ -20,6 +29,11 @@ const CourseCard = props => {
       <p>
         <strong>Description:</strong> {course.coursedescription}
       </p>
+      <CirclePicker
+        colors={['#673ab7', '#3f51b5', '#ffc107', '#ff9800', '#ff5722']}
+        onChangeComplete={handleChangeComplete}
+        circleSize={36}
+      />
       {props.children}
     </Card>
   );
