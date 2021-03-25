3b592a0c6495a8d571a376d387d30b2a1c7f8d66
martinezmarina <martinezmarina980@gmail.com>
2021-03-25 15:05:32 -0400

    draft pull request with changes to modules

--- src/components/RestModules/ModuleCard.js
+++ src/components/RestModules/ModuleCard.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Card } from 'antd';
+// import { Card } from 'antd';
+import { Table } from 'antd';
 
 const ModuleCard = props => {
   const { module } = props;
+
+  const dataSource = [
+    {
+      modulename: module.modulename,
+      description: module.moduledescription,
+    },
+  ];
+  const columns = [
+    {
+      title: 'Module Name',
+      dataIndex: 'modulename',
+      key: 'modulename',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+    },
+  ];
   return (
     <>
-      <Card title={module.modulename}>
-        <h3>Description: {module.moduledescription}</h3>
-        {/* PLEASE DO NOT REMOVE THE FOLLOWING LINE */}
-        {props.children}
-      </Card>
+      {/* <Card title={module.modulename}>
+        <h3>Description: {module.moduledescription}</h3> */}
+      {/* PLEASE DO NOT REMOVE THE FOLLOWING LINE */}
+      {/* {props.children}
+      </Card> */}
+      <Table columns={columns} dataSource={dataSource} />
     </>
   );
 };
