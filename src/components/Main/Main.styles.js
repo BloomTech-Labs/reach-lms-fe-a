8185e7226e94fbc924f52a6ff1d81426af831582
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-03-29 15:57:34 -0400

    add super simple searchbar to Main

--- src/components/Main/Main.styles.js
+++ src/components/Main/Main.styles.js
@@ -15,9 +15,17 @@ const Header = styled.div`
   width: 30vw;
 `;
 
+const SearchContainer = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Styled = {
   Content,
   Header,
+  SearchContainer,
 };
 
 export default Styled;
