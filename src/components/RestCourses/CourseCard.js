a038a2ab693579608061cd7a3e83424830202486
Sebastian <sebastianmendoza482@gmail.com>
2021-03-29 11:57:09 -0700

    fast commit

--- src/components/RestCourses/CourseCard.js
+++ src/components/RestCourses/CourseCard.js
@@ -2,21 +2,9 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { Card, Tag } from 'antd';
 import '../../styles/CourseCard.css';
-<<<<<<< HEAD
 import { CirclePicker } from 'react-color';
 import { useState } from 'react';
 import Tags from './Tags';
-=======
-import styled from 'styled-components';
-
-const StyledHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: flex-end;
-  width: 100%;
-`;
-
->>>>>>> 1edfee02e1656e2272e2be96edb20bb59d0b5821
 const CourseCard = props => {
   const { course } = props;
   const [color, setColor] = useState('');
@@ -27,7 +15,6 @@ const CourseCard = props => {
     <Card
       actions={true}
       title={
-<<<<<<< HEAD
         <div className="title">
           <h3>
             <strong>{course.coursename}</strong>
@@ -40,12 +27,6 @@ const CourseCard = props => {
           <Tags color={color} />
           <Tag color={color}>Hello</Tag>
         </div>
-=======
-        <StyledHeader>
-          <strong>{course.coursename}</strong>
-          {props.children}
-        </StyledHeader>
->>>>>>> 1edfee02e1656e2272e2be96edb20bb59d0b5821
       }
       className="course-card"
     >
