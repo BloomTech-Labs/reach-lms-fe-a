c1b93b570e72250bbe27267163105d00aa6028f4
Sebastian <sebastianmendoza482@gmail.com>
2021-04-01 16:45:40 -0700

    added render

--- src/components/common/Tags.js
+++ src/components/common/Tags.js
@@ -68,3 +68,93 @@ class ColorPickerUtility extends React.Component {
       saveEditInputRef = input => {
         this.editInput = input;
       };
+
+  render() {
+    const {
+      tags,
+      inputVisible,
+      inputValue,
+      editInputIndex,
+      editInputValue,
+    } = this.state;
+    return (
+      <>
+        <CirclePicker
+          colors={['#673ab7', '#3f51b5', '#ffc107', '#ff9800', '#ff5722']}
+          circleSize={20}
+          onChangeComplete={this.handleColorChange}
+        />
+        {tags.map((tag, index) => {
+          if (editInputIndex === index) {
+            return (
+              <Input
+                ref={this.saveEditInputRef}
+                key={tag}
+                size="small"
+                className="tag-input"
+                value={editInputValue}
+                onChange={this.handleEditInputChange}
+                onBlur={this.handleEditInputConfirm}
+                onPressEnter={this.handleEditInputConfirm}
+              />
+            );
+          }
+
+          const isLongTag = tag.title.length > 20;
+
+          const tagElem = (
+            <Tag
+              className="edit-tag"
+              key={tag.title}
+              closable={index !== 0}
+              onClose={() => this.handleClose(tag)}
+              color={tag.hexcode}
+            >
+              <span
+                onDoubleClick={e => {
+                  if (index !== 0) {
+                    this.setState(
+                      { editInputIndex: index, editInputValue: tag.title },
+                      () => {
+                        this.editInput.focus();
+                      }
+                    );
+                    e.preventDefault();
+                  }
+                }}
+              >
+                {isLongTag ? `${tag.title.slice(0, 20)}...` : tag.title}
+              </span>
+            </Tag>
+          );
+          return isLongTag ? (
+            <Tooltip title={tag.title} key={tag.title}>
+              {tagElem}
+            </Tooltip>
+          ) : (
+            tagElem
+          );
+        })}
+        {inputVisible && (
+          <Input
+            ref={this.saveInputRef}
+            type="text"
+            size="small"
+            className="tag-input"
+            value={inputValue}
+            onChange={this.handleInputChange}
+            onBlur={this.handleInputConfirm}
+            onPressEnter={this.handleInputConfirm}
+          />
+        )}
+        {!inputVisible && (
+          <Tag className="site-tag-plus" onClick={this.showInput}>
+            <PlusOutlined /> New Tag
+          </Tag>
+        )}
+      </>
+    );
+  }
+}
+
+export default ColorPickerUtility;
