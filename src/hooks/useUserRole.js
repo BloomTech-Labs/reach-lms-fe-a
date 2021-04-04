d37482255e4445a597a92ffee231f35754d10702
Simone Ballard <simone.a.ballard@gmail.com>
2021-04-03 21:58:33 -0600

    pushing combined changes of me and chaz

--- src/hooks/useUserRole.js
+++ src/hooks/useUserRole.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import { useRestfulFetch } from './useRestfulFetch';
 
-/**
- * this React hook allows the calling component the agency to check
- * whether the user signed into the app is a ADMIN, TEACHER, or STUDENT, respectively.
- *
- * The calling component will be provided with an Object containing three properties.
- * Each of those properties will hold a helper function that returns a boolean value
- * to denote whether the user signed in is a certain role.
- *
- * Additionally, we'll include the user data itself for ease of access to user information in any component that wants it.
- */
 export const useUserRole = () => {
   // const { data, status } = useUserContext();
   const { data, status } = useRestfulFetch('/users/getuserinfo');
